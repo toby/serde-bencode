8b9bf5cd71b822993b10753c3e84cc63091507da
Thomas Heck <t@b128.net>
2017-05-31 08:48:44 +0200

    cleanup tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -4,39 +4,27 @@ extern crate serde_derive;
 extern crate serde_bencode;
 
 use serde_bencode::value::Value;
-use serde_bencode::de::{self, from_bytes};
-use serde_bencode::ser::Serializer;
+use serde_bencode::de::{from_bytes, from_str};
+use serde_bencode::ser::{Serializer, to_bytes, to_string};
 use serde_bencode::error::Result;
 use serde::de::DeserializeOwned;
 use serde::ser::Serialize;
-use std::str::FromStr;
 use std::collections::HashMap;
 use std::fmt::Debug;
 
-fn encode<T: Serialize>(b: &T) -> Vec<u8> {
-    let mut ser = Serializer::new();
-    b.serialize(&mut ser).unwrap();
-    ser.into_vec()
-}
-
-fn decode_enum(s: &String) -> Option<Value> {
-    match de::from_str(&s.as_str()) {
-        Ok(r) => Some(r),
-        _ => None,
-    }
-}
-
-fn test_enum_enc_dec<T: Into<Value>>(a: T) {
+fn test_value_ser_de<T: Into<Value>>(a: T) {
     let a = a.into();
-    let a_bytes = encode(&a);
-    let b: Value = from_bytes(a_bytes.as_ref()).unwrap();
+    let mut ser = Serializer::new();
+    a.serialize(&mut ser).unwrap();
+    println!("bytes: {:?}", String::from_utf8_lossy(ser.as_ref()));
+    let b: Value = from_bytes(ser.as_ref()).unwrap();
     assert_eq!(a, b);
 }
 
-fn test_enum_dec_enc(s: &String) {
-    let d = decode_enum(s);
-    let e = encode(&d.unwrap());
-    assert_eq!(&s.as_bytes().to_vec(), &e);
+fn test_value_de_ser(s: &str) {
+    let d: Value = from_str(s).unwrap();
+    let e = to_string(&d).unwrap();
+    assert_eq!(s, e);
 }
 
 fn test_ser_de_eq<T>(a: T)
@@ -50,25 +38,25 @@ fn test_ser_de_eq<T>(a: T)
 }
 
 #[test]
-fn enc_dec_int() {
-    test_enum_enc_dec(666i64);
+fn ser_de_int() {
+    test_value_ser_de(666i64);
 }
 
 #[test]
-fn enc_dec_string() {
-    test_enum_enc_dec(String::from_str("yoda").unwrap());
+fn ser_de_string() {
+    test_value_ser_de("yoda");
 }
 
 #[test]
-fn enc_dec_enum_list_mixed() {
-    test_enum_enc_dec(Value::List(vec!["one".into(), "two".into(), "three".into(), 4i64.into()]));
+fn ser_de_value_list_mixed() {
+    test_value_ser_de(Value::List(vec!["one".into(), "two".into(), "three".into(), 4i64.into()]));
 }
 
 #[test]
-fn enc_dec_enum_list_nested() {
+fn ser_de_value_list_nested() {
     let l_grandchild = Value::List(vec!["two".into()]);
     let l_child = Value::List(vec!["one".into(), l_grandchild]);
-    test_enum_enc_dec(vec!["one".into(),
+    test_value_ser_de(vec!["one".into(),
                            "two".into(),
                            "three".into(),
                            4i64.into(),
@@ -76,14 +64,14 @@ fn enc_dec_enum_list_nested() {
 }
 
 #[test]
-fn enc_dec_map() {
+fn ser_de_value_map() {
     let mut m = HashMap::new();
     m.insert("Mc".into(), "Burger".into());
-    test_enum_enc_dec(m);
+    test_value_ser_de(m);
 }
 
 #[test]
-fn enc_dec_map_enum_mixed() {
+fn ser_de_map_value_mixed() {
     let mut ma = HashMap::new();
     ma.insert("M jr.".into(), "nuggets".into());
     let s = Value::List(vec!["one".into(), "two".into(), "three".into(), 4i64.into()]);
@@ -91,76 +79,47 @@ fn enc_dec_map_enum_mixed() {
     m.insert("Mc".into(), "Burger".into());
     m.insert("joint".into(), ma.into());
     m.insert("woah".into(), s);
-    test_enum_enc_dec(m);
+    test_value_ser_de(m);
 }
 
 #[test]
 fn serialize_i64() {
-    let x: i64 = 666;
-    let mut ser = Serializer::new();
-    x.serialize(&mut ser).unwrap();
-    let r: Vec<u8> = ser.into_vec();
-    assert_eq!(r, b"i666e");
+    assert_eq!(to_string(&666i64).unwrap(), "i666e");
 }
 
 #[test]
 fn serialize_str() {
-    let x = "xxx";
-    let mut ser = Serializer::new();
-    x.serialize(&mut ser).unwrap();
-    let r: Vec<u8> = ser.into_vec();
-    assert_eq!(r, b"3:xxx");
+    assert_eq!(to_string(&"xxx").unwrap(), "3:xxx");
 }
 
 #[test]
 fn serialize_bool() {
-    let x = false;
-    let mut ser = Serializer::new();
-    x.serialize(&mut ser).unwrap();
-    let r: Vec<u8> = ser.into_vec();
-    assert_eq!(r, b"i0e");
-
-    let x = true;
-    let mut ser = Serializer::new();
-    x.serialize(&mut ser).unwrap();
-    let r: Vec<u8> = ser.into_vec();
-    assert_eq!(r, b"i1e");
+    assert_eq!(to_string(&false).unwrap(), "i0e");
+    assert_eq!(to_string(&true).unwrap(), "i1e");
 }
 
 #[test]
 fn deserialize_to_string() {
-    let s = "3:yes";
-    let r: Result<String> = de::from_str(&s);
-    match r {
-        Ok(v) => assert_eq!(v, "yes"),
-        _ => panic!(),
-    }
+    let r: String = from_str("3:yes").unwrap();
+    assert_eq!(r, "yes");
 }
 
 #[test]
 fn deserialize_to_i64() {
-    let s = "i666e";
-    let r: Result<i64> = de::from_str(&s);
-    match r {
-        Ok(v) => assert_eq!(v, 666),
-        _ => panic!(),
-    }
+    let r: i64 = from_str("i666e").unwrap();
+    assert_eq!(r, 666);
 }
 
 #[test]
 fn deserialize_to_vec() {
-    let s = "li666ee";
-    let r: Result<Vec<i64>> = de::from_str(&s);
-    match r {
-        Ok(v) => assert_eq!(v, [666]),
-        _ => panic!(),
-    }
+    let r: Vec<i64> = from_str("li666ee").unwrap();
+    assert_eq!(r, [666]);
 }
 
 #[test]
 fn deserialize_to_freestyle() {
     let s = "li666e4:wontd3:onei666e4:yoyoli69ei89e4:yoyoeee";
-    test_enum_dec_enc(&String::from_str(s).unwrap());
+    test_value_de_ser(s);
 }
 
 #[test]
@@ -168,24 +127,28 @@ fn deserialize_to_freestyle() {
 #[should_panic(expected = "assertion failed")]
 fn trailing_chars() {
     let s = "i666ed";
-    let r = decode_enum(&String::from_str(s).unwrap());
-    assert!(r != None);
+    let r: Result<Value> = from_str(s);
+    assert!(r.is_err());
 }
 
 #[test]
 fn stop_short() {
     let s = "3:we";
-    let r = decode_enum(&String::from_str(s).unwrap());
-    assert_eq!(r, None);
+    let r: Result<Value> = from_str(s);
+    assert!(r.is_err());
 }
 
 #[test]
 fn multi_digit_string_length() {
-    assert_eq!(de::from_str::<String>(&String::from_str("1:o").unwrap()).unwrap(),
-               "o".to_owned());
-    assert_eq!(de::from_str::<String>(&String::from_str("10:oooooooooo").unwrap()).unwrap(),
-               "oooooooooo".to_owned());
-    assert_eq!(de::from_str::<String>(&String::from_str("100:oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo").unwrap()).unwrap(), "oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo".to_owned());
+    let s: String = from_str("1:o").unwrap();
+    assert_eq!(s, "o");
+
+    let s: String = from_str("10:oooooooooo").unwrap();
+    assert_eq!(s, "oooooooooo");
+
+    let s: String = from_str("100:oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo").unwrap();
+    assert_eq!(s,
+               "oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
 }
 
 #[test]
@@ -197,32 +160,29 @@ fn serialize_struct() {
     };
     let f = Fake {
         x: 1111,
-        y: String::from("dog"),
+        y: "dog".to_string(),
     };
-    let mut ser = Serializer::new();
-    f.serialize(&mut ser).unwrap();
-    let r: Vec<u8> = ser.into_vec();
-    assert_eq!(String::from_utf8(r).unwrap(), "d1:xi1111e1:y3:doge");
+    assert_eq!(to_string(&f).unwrap(), "d1:xi1111e1:y3:doge");
 }
 
 #[test]
 fn deserialize_to_struct() {
-    let b = "d1:xi1111e1:y3:doge".as_bytes();
+    let b = "d1:xi1111e1:y3:doge";
     #[derive(PartialEq, Debug, Deserialize)]
     struct Fake {
         y: String,
         x: i64,
     };
-    assert_eq!(de::from_bytes::<Fake>(b).unwrap(),
+    assert_eq!(from_str::<Fake>(b).unwrap(),
                Fake {
                    x: 1111,
-                   y: String::from_str("dog").unwrap(),
+                   y: "dog".to_string(),
                });
 }
 
 #[test]
 fn deserialize_to_struct_with_option() {
-    let b = "d1:xi1111e1:y3:dog1:z2:yoe".as_bytes();
+    let b = "d1:xi1111e1:y3:dog1:z2:yoe";
     #[derive(PartialEq, Debug, Deserialize)]
     struct Fake {
         y: String,
@@ -232,26 +192,20 @@ fn deserialize_to_struct_with_option() {
         #[serde(default)]
         a: Option<String>,
     };
-    let r: Result<Fake>;
-    r = de::from_bytes(b);
-    match r {
-        Ok(r) => {
-            assert_eq!(r,
-                       Fake {
-                           x: 1111,
-                           y: String::from_str("dog").unwrap(),
-                           z: Some(String::from_str("yo").unwrap()),
-                           a: None,
-                       })
-        }
-        Err(e) => panic!("Error: {:?}", e),
-    }
+    let r: Fake = from_str(b).unwrap();
+    assert_eq!(r,
+               Fake {
+                   x: 1111,
+                   y: "dog".to_string(),
+                   z: Some("yo".to_string()),
+                   a: None,
+               });
 }
 
 #[test]
 fn deserialize_to_value() {
-    let b = "d1:xi1111e1:y3:doge".as_bytes();
-    let r: Value = de::from_bytes(b).unwrap();
+    let b = "d1:xi1111e1:y3:doge";
+    let r: Value = from_str(b).unwrap();
     let mut d = HashMap::new();
     d.insert("x".into(), 1111.into());
     d.insert("y".into(), "dog".into());
@@ -260,7 +214,7 @@ fn deserialize_to_value() {
 
 #[test]
 fn deserialize_to_value_struct_mix() {
-    let b = "d1:xi1111e1:y3:dog1:zi66e1:qli666eee".as_bytes();
+    let b = "d1:xi1111e1:y3:dog1:zi66e1:qli666eee";
     #[derive(PartialEq, Debug, Deserialize)]
     struct Fake {
         y: String,
@@ -268,20 +222,14 @@ fn deserialize_to_value_struct_mix() {
         z: Value,
         q: Vec<i64>,
     };
-    let r: Result<Fake>;
-    r = de::from_bytes(b);
-    match r {
-        Ok(r) => {
-            assert_eq!(r,
-                       Fake {
-                           x: 1111,
-                           y: String::from_str("dog").unwrap(),
-                           z: Value::Int(66),
-                           q: vec![666],
-                       })
-        }
-        Err(e) => panic!("Error: {:?}", e),
-    }
+    let r: Fake = from_str(b).unwrap();
+    assert_eq!(r,
+               Fake {
+                   x: 1111,
+                   y: "dog".to_string(),
+                   z: Value::Int(66),
+                   q: vec![666],
+               });
 }
 
 #[test]
@@ -299,11 +247,7 @@ fn serialize_lexical_sorted_keys() {
         z: 3,
         c: 4,
     };
-    let mut ser = Serializer::new();
-    f.serialize(&mut ser).unwrap();
-    let r: Vec<u8> = ser.into_vec();
-    assert_eq!(String::from_utf8(r).unwrap(),
-               "d3:aaai1e2:bbi2e1:ci4e1:zi3ee");
+    assert_eq!(to_string(&f).unwrap(), "d3:aaai1e2:bbi2e1:ci4e1:zi3ee");
 }
 
 #[test]
@@ -311,37 +255,25 @@ fn serialize_newtype_struct() {
     #[derive(Serialize)]
     struct Fake(i32);
     let f = Fake(66);
-    let mut ser = Serializer::new();
-    f.serialize(&mut ser).unwrap();
-    let r: Vec<u8> = ser.into_vec();
-    assert_eq!(String::from_utf8(r).unwrap(), "i66e");
+    assert_eq!(to_string(&f).unwrap(), "i66e");
 }
 
 #[test]
 fn serialize_some() {
     let f = Some(1);
-    let mut ser = Serializer::new();
-    f.serialize(&mut ser).unwrap();
-    let r: Vec<u8> = ser.into_vec();
-    assert_eq!(String::from_utf8(r).unwrap(), "i1e");
+    assert_eq!(to_string(&f).unwrap(), "i1e");
 }
 
 #[test]
 fn serialize_none() {
     let f: Option<Value> = None;
-    let mut ser = Serializer::new();
-    f.serialize(&mut ser).unwrap();
-    let r: Vec<u8> = ser.into_vec();
-    assert_eq!(String::from_utf8(r).unwrap(), "");
+    assert_eq!(to_string(&f).unwrap(), "");
 }
 
 #[test]
 fn serialize_tuple() {
     let f = (1, 2, 3, "one");
-    let mut ser = Serializer::new();
-    f.serialize(&mut ser).unwrap();
-    let r: Vec<u8> = ser.into_vec();
-    assert_eq!(String::from_utf8(r).unwrap(), "li1ei2ei3e3:onee");
+    assert_eq!(to_string(&f).unwrap(), "li1ei2ei3e3:onee");
 }
 
 #[test]
@@ -349,20 +281,13 @@ fn serialize_tuple_struct() {
     #[derive(Serialize)]
     struct Fake(i32, i32);
     let f = Fake(66, 66);
-    let mut ser = Serializer::new();
-    f.serialize(&mut ser).unwrap();
-    let r: Vec<u8> = ser.into_vec();
-    assert_eq!(String::from_utf8(r).unwrap(), "li66ei66ee");
+    assert_eq!(to_string(&f).unwrap(), "li66ei66ee");
 }
 
 #[test]
 fn readme_value_example() {
     let list: Vec<Value> = vec!["one".into(), "two".into(), "three".into(), 4i64.into()];
-    let mut ser = Serializer::new();
-    list.serialize(&mut ser).unwrap();
-    let list_serialize: Vec<u8> = ser.into_vec();
-    assert_eq!(String::from_utf8(list_serialize).unwrap(),
-               "l3:one3:two5:threei4ee");
+    assert_eq!(to_string(&list).unwrap(), "l3:one3:two5:threei4ee");
 }
 
 #[test]
@@ -376,10 +301,7 @@ fn struct_none_vals() {
         a: None,
         b: Some(1),
     };
-    let mut ser = Serializer::new();
-    f.serialize(&mut ser).unwrap();
-    let r: Vec<u8> = ser.into_vec();
-    assert_eq!(String::from_utf8(r).unwrap(), "d1:bi1ee");
+    assert_eq!(to_string(&f).unwrap(), "d1:bi1ee");
 }
 
 #[test]
@@ -424,4 +346,9 @@ fn ser_de_variant_struct() {
     };
     test_ser_de_eq(Mock::A { a: 123, b: 321 });
     test_ser_de_eq(Mock::B { c: 321, d: 123 });
+}
+
+#[test]
+fn test_to_bytes() {
+    assert_eq!(to_bytes(&"test").unwrap(), b"4:test");
 }
