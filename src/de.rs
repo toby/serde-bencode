7c419ecd86d356bd4c471fbd2b56e15950af01b7
TQ Hirsch <tq@p3ki.com>
2018-02-23 13:08:54 +0100

    Made newtype struct deserialization work with derived instances

--- src/de.rs
+++ src/de.rs
@@ -245,7 +245,12 @@ impl<'de, 'a, R: Read> de::Deserializer<'de> for &'a mut Deserializer<R> {
     forward_to_deserialize_any! {
         i64 string seq bool i8 i16 i32 u8 u16 u32
         u64 f32 f64 char str unit bytes byte_buf map unit_struct tuple_struct tuple
-        newtype_struct ignored_any identifier struct
+        ignored_any identifier struct
+    }
+
+    #[inline]
+    fn deserialize_newtype_struct<V: de::Visitor<'de>>(self, _name: &'static str, visitor: V) -> Result<V::Value> {
+        visitor.visit_newtype_struct(self)
     }
 
     #[inline]
