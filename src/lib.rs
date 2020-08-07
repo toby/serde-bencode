d53cb25de2290f9f501bae344fb1ffb0d941c5f2
TopologicallySpeaking <64005627+TopologicallySpeaking@users.noreply.github.com>
2020-08-07 04:02:28 -0400

    Add top-level module documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,41 @@
+//! This crate is a Rust library for using the [Serde](https://github.com/serde-rs/serde)
+//! serialization framework with bencode data.
+//!
+//! # Examples
+//!
+//! ```
+//! use serde_derive::{Serialize, Deserialize};
+//!
+//! #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
+//! struct Product {
+//!     name: String,
+//!     price: u32,
+//! }
+//!
+//! fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     let apple = Product {
+//!         name: "Apple".to_string(),
+//!         price: 130,
+//!     };
+//!
+//!     let serialized = serde_bencode::to_string(&apple)?;
+//!
+//!     assert_eq!(serialized, "d4:name5:Apple5:pricei130ee".to_string());
+//!
+//!     let deserialized: Product = serde_bencode::from_str(&serialized)?;
+//!
+//!     assert_eq!(
+//!         deserialized,
+//!         Product {
+//!             name: "Apple".to_string(),
+//!             price: 130,
+//!         }
+//!     );
+//!
+//!     Ok(())
+//! }
+//! ```
+
 pub mod error;
 pub mod ser;
 pub mod de;
